9014cfe987f22dffdbc8ee75f6c18fb980d34791
xuoplex <3125565277@qq.com>
2024-04-09 21:23:50 +0800

    修改日志功能

--- agency_swarm/util/oai.py
+++ agency_swarm/util/oai.py
@@ -17,7 +17,7 @@ def get_openai_client():
         if client is None:
             # Check if the API key is set
             api_key = openai.api_key or os.getenv('OPENAI_API_KEY')
-            url =  openai.base_url or os.getenv('OPENAI_API_URL') or 'https://api.openai.com'
+            url =  openai.base_url or os.getenv('OPENAI_BASE_URL')
             if api_key is None:
                 raise ValueError("OpenAI API key is not set. Please set it using set_openai_key.")
             client = instructor.patch(openai.OpenAI(api_key=api_key,
